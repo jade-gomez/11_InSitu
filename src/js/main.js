1fa28892c6f2b7d991794ab3387fca11b1715b91
Jadegomez <jade.gomez@studentfr.ch>
2025-05-08 10:51:54 +0200

    base cube

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 import * as THREE from "three";
+var element = document.querySelector(".element");
+var width = element.clientWidth;
+var height = element.clientHeight;
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+camera.position.z = 5;
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+element.appendChild(renderer.domElement);
+renderer.render(scene, camera);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
